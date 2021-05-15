bb6727cc4adb06097d9a56503d38863090dc8b86
Dillon Nys <dnys1@asu.edu>
2021-05-14 18:09:14 -0700

    Add card padding

--- app/lib/ui/package_card_view.dart
+++ app/lib/ui/package_card_view.dart
@@ -11,77 +11,80 @@ class PackageCardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints.tightFor(width: 400),
-      child: Card(
-        shadowColor: Colors.black26,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Wrap(
-                children: [
-                  MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: Link(
-                      uri: PubService.packageUrl(package.name),
-                      target: LinkTarget.blank,
-                      builder: (context, FollowLink launch) {
-                        return GestureDetector(
-                          onTap: launch,
-                          child: Text(package.name, style: linkTitleTextStyle),
-                        );
-                      },
-                    ),
-                  ),
-                  if (package.isNullSafe) ...const [
-                    SizedBox(width: 5),
-                    NullSafetyBadge(),
-                  ]
-                ],
-              ),
-              SelectableText(
-                package.description,
-                style: const TextStyle(color: textPrimaryColor, fontSize: 14, height: 1.6),
-                maxLines: 3,
-              ),
-              const SizedBox(height: 16),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  if (package.hasPublisher) ...[
-                    verifiedPublisherImage,
-                    const SizedBox(width: 4),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ConstrainedBox(
+        constraints: const BoxConstraints.tightFor(width: 400),
+        child: Card(
+          shadowColor: Colors.black26,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Wrap(
+                  children: [
                     MouseRegion(
                       cursor: SystemMouseCursors.click,
                       child: Link(
-                        uri: PubService.publisherUrl(package.publisher),
+                        uri: PubService.packageUrl(package.name),
                         target: LinkTarget.blank,
                         builder: (context, FollowLink launch) {
                           return GestureDetector(
                             onTap: launch,
-                            child: Text(package.publisher, style: TextStyle(color: linkColor, fontSize: 12)),
+                            child: Text(package.name, style: linkTitleTextStyle),
                           );
                         },
                       ),
                     ),
+                    if (package.isNullSafe) ...const [
+                      SizedBox(width: 5),
+                      NullSafetyBadge(),
+                    ]
                   ],
-                  const Spacer(),
-                  Row(
-                    children: [
-                      if (package.isDartPackage) ...[
-                        dartLogo,
-                        const SizedBox(width: 4),
-                      ],
-                      if (package.isFlutterPackage) flutterLogo,
+                ),
+                SelectableText(
+                  package.description,
+                  style: const TextStyle(color: textPrimaryColor, fontSize: 14, height: 1.6),
+                  maxLines: 3,
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    if (package.hasPublisher) ...[
+                      verifiedPublisherImage,
+                      const SizedBox(width: 4),
+                      MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Link(
+                          uri: PubService.publisherUrl(package.publisher),
+                          target: LinkTarget.blank,
+                          builder: (context, FollowLink launch) {
+                            return GestureDetector(
+                              onTap: launch,
+                              child: Text(package.publisher, style: TextStyle(color: linkColor, fontSize: 12)),
+                            );
+                          },
+                        ),
+                      ),
                     ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8),
-            ],
+                    const Spacer(),
+                    Row(
+                      children: [
+                        if (package.isDartPackage) ...[
+                          dartLogo,
+                          const SizedBox(width: 4),
+                        ],
+                        if (package.isFlutterPackage) flutterLogo,
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+              ],
+            ),
           ),
         ),
       ),
